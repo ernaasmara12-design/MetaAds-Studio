ece6555bfa129e6c52b7b0e680dc95063ea72d52
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-25 08:08:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
+"""
+Meta Ads Studio Configuration
+"""
+
+import os
+
 APP_NAME = "MetaAds SaaS"
 VERSION = "1.0.0"
+
+APP_ID = os.getenv("META_APP_ID", "")
+APP_SECRET = os.getenv("META_APP_SECRET", "")
+ACCESS_TOKEN = os.getenv("META_ACCESS_TOKEN", "")
+AD_ACCOUNT_ID = os.getenv("META_AD_ACCOUNT_ID", "")
+API_VERSION = os.getenv("META_API_VERSION", "v24.0")
