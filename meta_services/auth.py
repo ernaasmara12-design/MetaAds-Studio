92124f753b37dcb09b8f515ae382c0d96c10bec4
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-25 08:58:53 +0700

    Update auth.py

--- meta_services/auth.py
+++ meta_services/auth.py
@@ -7,21 +7,16 @@ from facebook_business.api import FacebookAdsApi
 
 class MetaAuth:
 
-    def __init__(
-        self,
+    @staticmethod
+    def connect(
         app_id: str,
         app_secret: str,
         access_token: str,
     ):
-        self.app_id = app_id
-        self.app_secret = app_secret
-        self.access_token = access_token
-
-    def connect(self):
         FacebookAdsApi.init(
-            app_id=self.app_id,
-            app_secret=self.app_secret,
-            access_token=self.access_token,
+            app_id=app_id,
+            app_secret=app_secret,
+            access_token=access_token,
         )
 
-        return FacebookAdsApi.get_default_api()
+        return True
