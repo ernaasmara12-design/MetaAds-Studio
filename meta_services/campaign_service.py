6b8d994e2d443a7ec0b0b36074ce5f87b6231a34
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-25 07:55:08 +0700

    Update campaign_service.py

--- meta_services/campaign_service.py
+++ meta_services/campaign_service.py
@@ -11,3 +11,12 @@ class CampaignService:
 
     def __init__(self, account_id: str):
         self.account = AdAccount(account_id)
+
+    def create_campaign(self, data: dict):
+        payload = build_campaign_payload(data)
+
+        campaign = self.account.create_campaign(
+            params=payload
+        )
+
+        return campaign
