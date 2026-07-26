e61adbe85d9d5cf37724679623dc89e00316f3f2
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-26 10:20:22 +0700

    Create cta.py

---
+++ meta_enums/cta.py
@@ -0,0 +1,12 @@
+CALL_TO_ACTIONS = {
+    "Learn More": "LEARN_MORE",
+    "Shop Now": "SHOP_NOW",
+    "Sign Up": "SIGN_UP",
+    "Book Now": "BOOK_TRAVEL",
+    "Download": "DOWNLOAD",
+    "Contact Us": "CONTACT_US",
+    "Apply Now": "APPLY_NOW",
+    "Send Message": "MESSAGE_PAGE",
+    "Send WhatsApp": "WHATSAPP_MESSAGE",
+    "Call Now": "CALL_NOW",
+}
