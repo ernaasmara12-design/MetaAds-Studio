6242c26a7a7ac702137257c219dd6223aa39e38e
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-26 14:50:32 +0700

    Create optimization_goals.py

---
+++ meta_enums/optimization_goals.py
@@ -0,0 +1,14 @@
+OPTIMIZATION_GOALS = {
+    "Reach": "REACH",
+    "Impressions": "IMPRESSIONS",
+    "Link Clicks": "LINK_CLICKS",
+    "Landing Page Views": "LANDING_PAGE_VIEWS",
+    "Post Engagement": "POST_ENGAGEMENT",
+    "Conversions": "OFFSITE_CONVERSIONS",
+    "Value": "VALUE",
+    "Purchase": "PURCHASE",
+    "Leads": "LEADS",
+    "App Installs": "APP_INSTALLS",
+    "Quality Leads": "QUALITY_LEAD",
+    "ThruPlay": "THRUPLAY"
+}
