5dfc4a8b7d9156bf990aa5b7c0f7de05802a8e51
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-26 12:04:50 +0700

    Update adset_payload.py

--- meta_payloads/adset_payload.py
+++ meta_payloads/adset_payload.py
@@ -1,74 +1,24 @@
-from datetime import datetime
-
 from facebook_business.adobjects.adset import AdSet
 
-from meta_enums.adset import (
-    BILLING_EVENTS,
-    OPTIMIZATION_GOALS,
-)
+from meta_builders.budget_builder import build_budget
+from meta_builders.optimization_builder import build_optimization
+from meta_builders.schedule_builder import build_schedule
 
 
 def build_adset_payload(data):
 
     payload = {
+
         AdSet.Field.name: data["adset_name"],
+
         AdSet.Field.campaign_id: data["campaign_id"],
+
         AdSet.Field.status: "PAUSED",
+
     }
 
-    # ======================================
-    # Budget
-    # ======================================
-
-    if data["budget_type"] == "Daily Budget":
-        payload[AdSet.Field.daily_budget] = int(data["budget"] * 100)
-    else:
-        payload[AdSet.Field.lifetime_budget] = int(data["budget"] * 100)
-
-    # ======================================
-    # Billing Event
-    # ======================================
-
-    billing_event = BILLING_EVENTS.get(
-        data.get("billing_event")
-    )
-
-    if billing_event:
-        payload[AdSet.Field.billing_event] = billing_event
-
-    # ======================================
-    # Optimization Goal
-    # ======================================
-
-    optimization_goal = OPTIMIZATION_GOALS.get(
-        data.get("optimization_goal")
-    )
-
-    if optimization_goal:
-        payload[AdSet.Field.optimization_goal] = optimization_goal
-
-    # ======================================
-    # Schedule
-    # ======================================
-
-    start_date = data.get("start_date")
-    start_time = data.get("start_time")
-
-    if start_date and start_time:
-
-        payload[AdSet.Field.start_time] = datetime.combine(
-            start_date,
-            start_time,
-        ).isoformat()
-
-    end_date = data.get("end_date")
-    end_time = data.get("end_time")
-
-    if end_date and end_time:
-
-        payload[AdSet.Field.end_time] = datetime.combine(
-            end_date,
-            end_time,
-        ).isoformat()
+    payload.update(build_budget(data))
+    payload.update(build_optimization(data))
+    payload.update(build_schedule(data))
 
     return payload
