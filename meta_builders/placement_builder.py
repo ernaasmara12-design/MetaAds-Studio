94c794d5ce52b31a4c154266989e0d0b68c63289
ernaasmara12-design <erna.asmara12@gmail.com>
2026-07-26 17:20:38 +0700

    Update placement_builder.py

--- meta_builders/placement_builder.py
+++ meta_builders/placement_builder.py
@@ -1,7 +1,9 @@
 from facebook_business.adobjects.adset import AdSet
+
 from meta_enums.meta_api_enums import (
     PUBLISHER_PLATFORMS,
 )
+
 
 def build_placement(data):
 
@@ -12,11 +14,11 @@ def build_placement(data):
 
     publisher_platforms = data.get("publisher_platforms", [])
 
-if publisher_platforms:
-    placement[AdSet.Field.publisher_platforms] = [
-        PUBLISHER_PLATFORMS.get(platform, platform)
-        for platform in publisher_platforms
-    ]
+    if publisher_platforms:
+        placement[AdSet.Field.publisher_platforms] = [
+            PUBLISHER_PLATFORMS.get(platform, platform)
+            for platform in publisher_platforms
+        ]
 
     if data.get("facebook_positions"):
         placement[AdSet.Field.facebook_positions] = data["facebook_positions"]
@@ -28,8 +30,6 @@ if publisher_platforms:
         placement[AdSet.Field.messenger_positions] = data["messenger_positions"]
 
     if data.get("audience_network_positions"):
-        placement[
-            AdSet.Field.audience_network_positions
-        ] = data["audience_network_positions"]
+        placement[AdSet.Field.audience_network_positions] = data["audience_network_positions"]
 
     return placement
